5954e14c19f4f8b5efdcd52d6a284a7477a9da9f
jason <jasonchinonso20@gmail.com>
2025-05-13 13:00:00 +0100

    Modified

--- app/(main)/cart/index.tsx
+++ app/(main)/cart/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function Cart() {
   return (
     <View className="flex-1 p-4">
-      <Text className="text-2xl font-bold">Shopping Cart</Text>
+      <Text className="text-2xl font-bold">Shopping Cartt</Text>
     </View>
   );
 }
