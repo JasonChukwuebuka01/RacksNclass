950f83f5f0491293dadf5bb67a813d87b7687fab
jason <jasonchinonso20@gmail.com>
2025-05-18 00:03:47 +0100

    Modified

--- app/checkout/index.tsx
+++ app/checkout/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function Checkout() {
   return (
     <View className="flex-1 p-4">
-      <Text className="text-2xl font-bold">Checkoutt</Text>
+      <Text className="text-2xl font-bold">Checkout</Text>
     </View>
   );
 }
